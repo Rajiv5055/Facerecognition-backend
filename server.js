65ae27be1bafe992bcebdbaa71a439804eb1f09d
Rajiv <rajeevnayan5055@gmail.com>
2021-08-12 23:06:15 +0530

    adding key

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
@@ -34,5 +35,5 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running on port $(process.env.PORT)`);
+  console.log(`app is running on port ${process.env.PORT}`);
 })
